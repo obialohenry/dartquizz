4e982860631c08addb8b9eb9d504506cff6d1465
sisu <obilaorchisomebi123@gmail.com>
2025-04-28 11:05:37 +0100

    Custom navigator class
    To handle navigation across entire app

---
+++ lib/utils/navigators.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+///This method ensures navigation and kills the previous activity
+navigateReplace(BuildContext context, Widget widget) {
+  Navigator.pushReplacement(context, FadeRoute(page: widget));
+} // navigateReplace(BuildContext context, Widget widget) {
+//   Navigator.pushReplacement(
+//       context, CupertinoPageRoute(builder: (context) => widget));
+// }
+
+///This method ensures navigation and does not kill the previous activity
+navigatePush(BuildContext context, Widget widget) {
+  Navigator.push(context, FadeRoute(page: widget));
+}
+// navigatePush(BuildContext context, Widget widget) {
+//   Navigator.push(context, CupertinoPageRoute(builder: (context) => widget));
+// }
+
+///This method backward navigation
+navigateBack(
+  BuildContext context,
+) {
+  Navigator.pop(context);
+}
+
+class FadeRoute<T> extends PageRouteBuilder<T> {
+  final Widget page;
+
+  FadeRoute({required this.page})
+      : super(
+          pageBuilder: (BuildContext context, Animation<double> animation,
+              Animation<double> secondaryAnimation) {
+            return page;
+          },
+          transitionsBuilder: (BuildContext context,
+              Animation<double> animation,
+              Animation<double> secondaryAnimation,
+              Widget child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+        );
+}
